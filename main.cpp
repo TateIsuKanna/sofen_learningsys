e18538fcb9213d62816afc2739acf8d9c6882cd2
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-12-27 16:19:25 +0900

    作ったはいいけど

--- main.cpp
+++ main.cpp
@@ -1,50 +1,37 @@
-#include<stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
 
-int main()
-{
-	FILE *fp;
-	char fname[20];
-	char save1[2][150]={};
-	char save2[2][150]={};
-	char c[5000][2][150]={};	//改行文字の関係で配列を3個使って終了判定をしています。
-	
-	
-	printf("開きたいファイル名を入力してください。\n");
-	scanf("%s",fname);
-	fp = fopen(fname,"r");
-	if(fp==NULL){
-		printf("ぬるぽ\n");
+struct problem{
+        string question;
+        string answer;
+        bool issucceeded;
+};
+
+int main(){
+	cout<<"開きたいファイル名を入力してください。"<<endl;
+	string file_name;
+        cin>>file_name;
+
+        string file_path="questions/"+file_name+".csv";
+        ifstream ifs(file_path);
+        if(!ifs){
+		cerr<<"ぬるぽ"<<"("<<file_path<<"なんてファイルないです)"<<endl;
 		return -1;
-	}
-	
-	int i,j,k;
-	i=1;
-	fscanf(fp,"%[^,],%s",c[0][0],c[0][1]);
-	fscanf(fp,"%[^,],%s",save1[0],save1[1]);
-	for(;;){
-		if(fscanf(fp,"%[^,],%s",save2[0],save2[1])==EOF){
-			break;
-		}
-		for(j=0;j<2;j++){
-			for(k=0;k<150;k++){
-				c[i][j][k]=save1[j][k];
-				save1[j][k]=save2[j][k];
-			}
-		}
-		i++;
-	}
-	
-	fclose(fp);
-	
-	for(i=0;i<5000;i++){		//以下出力
-		if(c[i][0][0]=='\0'){
-			break;
-		}
-		//printf("%s",c[i][5]);
-		printf("%s %s",c[i][0],c[i][1]);
-	}
-	printf("\n");
-	return 0;
+        }
+
+        vector<problem> problems;
+        while(!ifs.eof()){
+                problem t;
+                ifs >> t.question;
+                ifs >> t.answer;
+                t.issucceeded=false;//TODO:
+                problems.push_back(t);
+        }
+
+        for(auto t:problems){
+                cout<<t.question<<" "<<t.answer<<endl;
+        }
 }
