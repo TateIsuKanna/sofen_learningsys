c71355f20c9f775f74fe53f43cf3f1b992241711
aste-github <34529552+aste-github@users.noreply.github.com>
2017-12-31 15:13:43 +0900

    個人的:スペースをタブに

--- main.cpp
+++ main.cpp
@@ -7,42 +7,42 @@
 using namespace std;
 
 struct problem{
-        string question;
-        string answer;
-        bool issucceeded;
+	string question;
+	string answer;
+	bool issucceeded;
 };
 
 vector<int> make_rand_array(int min,int max){
-  if(min>max) swap(min,max);
-  vector<int> v;
-  for(int i=min;i<=max;i++) v.push_back(i);
-  random_device get_rand_mt;
-  shuffle(v.begin(),v.end(),get_rand_mt);
-  return v;
+	if(min>max) swap(min,max);
+	vector<int> v;
+	for(int i=min;i<=max;i++) v.push_back(i);
+	random_device get_rand_mt;
+	shuffle(v.begin(),v.end(),get_rand_mt);
+	return v;
 }
 
 int main(){
 	cout<<"開きたいファイル名を入力してください。"<<endl;
 	string file_name;
-        cin>>file_name;
+	cin>>file_name;
 
-        string file_path="questions/"+file_name+".csv";
-        ifstream ifs(file_path);
-        if(!ifs){
+	string file_path="questions/"+file_name+".csv";
+	ifstream ifs(file_path);
+	if(!ifs){
 		cerr<<"ぬるぽ"<<"("<<file_path<<"なんてファイルないです)"<<endl;
 		return -1;
-        }
+	}
 
-        vector<problem> problems;
-        while(!ifs.eof()){
-                problem t;
-                ifs >> t.question;
-                ifs >> t.answer;
-                t.issucceeded=false;//TODO:
-                problems.push_back(t);
-        }
+	vector<problem> problems;
+	while(!ifs.eof()){
+		problem t;
+		ifs >> t.question;
+		ifs >> t.answer;
+		t.issucceeded=false;//TODO:
+		problems.push_back(t);
+	}
 
-        for(auto t:problems){
-                cout<<t.question<<" "<<t.answer<<endl;
-        }
+	for(auto t:problems){
+		cout<<t.question<<" "<<t.answer<<endl;
+	}
 }
