c92d7d29070d2759131abc9379b830dc75d6178e
sorhnthrnkrm <r114309@g.kobe-kosen.ac.jp>
2018-01-18 12:07:03 +0900

    オメデトウAA

--- main.cpp
+++ main.cpp
@@ -1,5 +1,126 @@
-#include<iostream>
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void  main( )
+{
+printf("            ■                   \n");
+	printf("            ■               \n");
+	printf("  ■ ■ ■ ■ ■ ■ ■ ■ ■ \n");
+	printf("          ■ ■              \n");
+	printf("        ■   ■              \n");
+	printf("      ■     ■              \n");
+	printf("    ■       ■              \n");
+	printf(" ■          ■              \n");
+	usleep(500000);                          // 500000マイクロ秒(0.5秒)止まる
+
+	printf("            ■                       \n");
+	printf("            ■                          ■     \n");
+	printf("  ■ ■ ■ ■ ■ ■ ■ ■ ■       ■        \n");
+	printf("          ■ ■                ■    ■     \n");
+	printf("        ■   ■                  ■         \n");
+	printf("      ■     ■               ■   ■        \n");
+	printf("    ■       ■             ■       ■       \n");
+	printf(" ■          ■         ■               \n");
+	usleep(500000);                          // 500000マイクロ秒(0.5秒)止まる
+
+	printf("            ■                                               ■ ■     \n");
+	printf("            ■                          ■       ■ ■ ■ ■ ■        \n");
+	printf("  ■ ■ ■ ■ ■ ■ ■ ■ ■       ■         ■                       \n");
+	printf("          ■ ■                ■    ■        ■ ■ ■ ■ ■ ■ ■ ■ ■  \n");
+	printf("        ■   ■                  ■                   ■               \n");
+	printf("      ■     ■               ■   ■                 ■               \n");
+	printf("    ■       ■             ■       ■             ■                 \n");
+	printf(" ■          ■         ■                       ■                    \n");
+	usleep(500000);                          // 500000マイクロ秒(0.5秒)止まる
+
+	printf("            ■                                               ■ ■     ■            \n");
+	printf("            ■                          ■       ■ ■ ■ ■ ■            ■        \n");
+	printf("  ■ ■ ■ ■ ■ ■ ■ ■ ■       ■         ■                               ■    \n");
+	printf("          ■ ■                ■    ■        ■ ■ ■ ■ ■ ■ ■ ■ ■        ■ ■   \n");
+	printf("        ■   ■                  ■                   ■                ■     ■       \n");
+	printf("      ■     ■               ■   ■                 ■                 ■         ■  \n");
+	printf("    ■       ■             ■       ■             ■                   ■             \n");
+	printf(" ■          ■         ■                       ■                     ■              \n");
+	usleep(500000);                          // 500000マイクロ秒(0.5秒)止まる
+
+	printf("            ■                                               ■ ■     ■                    ■              \n");
+	printf("            ■                          ■       ■ ■ ■ ■ ■            ■                    ■             \n");
+	printf("  ■ ■ ■ ■ ■ ■ ■ ■ ■       ■         ■                               ■              ■ ■ ■ ■ ■ ■ ■     \n");
+	printf("          ■ ■                ■    ■        ■ ■ ■ ■ ■ ■ ■ ■ ■        ■ ■            ■           ■       \n");
+	printf("        ■   ■                  ■                   ■                ■     ■                   ■         \n");
+	printf("      ■     ■               ■   ■                 ■                 ■         ■             ■           \n");
+	printf("    ■       ■             ■       ■             ■                   ■                     ■               \n");
+	printf(" ■          ■         ■                       ■                     ■                   ■                \n");
+	usleep(500000);                          // 500000マイクロ秒(0.5秒)止まる
+
+	while ( 1 )                                 // 無限ループ
+	{
+	system("clear");                          // 画面消去
+	printf("            ■                                               ■ ■     ■                    ■                ■■    ■■\n");
+	printf("            ■                          ■       ■ ■ ■ ■ ■            ■                    ■                ■■    ■■\n");
+	printf("  ■ ■ ■ ■ ■ ■ ■ ■ ■       ■         ■                               ■              ■ ■ ■ ■ ■ ■ ■          ■■    ■■\n");
+	printf("          ■ ■                ■    ■        ■ ■ ■ ■ ■ ■ ■ ■ ■        ■ ■            ■           ■          ■■    ■■\n");
+	printf("        ■   ■                  ■                   ■                ■     ■                   ■           ■■    ■■\n");
+	printf("      ■     ■               ■   ■                 ■                 ■         ■             ■             ■■    ■■\n");
+	printf("    ■       ■             ■       ■             ■                   ■                     ■                           \n");
+	printf(" ■          ■         ■                       ■                     ■                   ■                 ■■    ■■\n");
+	usleep(500000);                          // 500000マイクロ秒(0.5秒)止まる
+
+	system("cls");                          // 画面消去
+	printf("                   \n");
+	printf("                ■ \n");
+	printf("    ■         ■  \n");
+	printf("       ■    ■    \n");
+	printf("         ■        \n");
+	printf("      ■   ■      \n");
+	printf("    ■       ■    \n");
+	printf("■                 \n");
+	usleep(500000);                             // 500000マイクロ秒(0.5秒)止まる
+      
+	system("cls");
+	printf("                   ■ ■\n  ");
+	printf("   ■ ■ ■ ■ ■       \n");
+	printf("                        \n");
+	printf(" ■ ■ ■ ■ ■ ■ ■ ■\n");
+	printf("         ■             \n");
+	printf("         ■             \n");
+	printf("       ■               \n");
+	printf("     ■                 \n");
+	usleep(500000);
+
+	system("cls");
+	printf("        ■           \n");
+	printf("        ■           \n");
+	printf("        ■           \n");
+	printf("        ■ ■        \n");
+	printf("        ■    ■     \n");
+	printf("        ■        ■ \n");
+	printf("        ■           \n");
+	printf("        ■           \n");
+	usleep(500000);
+
+	system("cls");
+	printf("         ■              \n");
+	printf("         ■              \n");
+	printf("  ■ ■ ■ ■ ■ ■ ■   \n");
+	printf("  ■           ■        \n");
+	printf("              ■         \n");
+	printf("            ■           \n");
+	printf("          ■             \n");
+	printf("        ■               \n");
+	usleep(500000);
+
+	system("cls");
+	printf("  ■■    ■■\n");
+	printf("  ■■    ■■\n");
+	printf("  ■■    ■■\n");
+	printf("  ■■    ■■\n");
+	printf("  ■■    ■■\n");
+	printf("  ■■    ■■\n");
+	printf("              \n");
+	printf("  ■■    ■■\n");
+	usleep(500000);
 }
 
-//こんにちは私の名前は織田崚平です
+
+}
